63baad96587e528965630418ab9519e742f68b26
victor shok <156413267+lokritshok@users.noreply.github.com>
2024-08-15 03:01:20 +0100

    assgmt func

---
+++ Basic JavaScript Projects/project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+//assigning two variables
+var Blue = ("I love Blue colours");
+var Green = (" and can mix it with an array of Olive green!");
+
+//document.write
+document.write(Blue + Green);
+
+//displaying a "p" element assigned an id name using the document.getElementById method
+document.getElementById (Blue_colours + Olive_green).innerHTML = str;
